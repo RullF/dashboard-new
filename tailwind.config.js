b935f0acd4269ae5827a344f1555d32cbca0919b
RullF <121843378+RullF@users.noreply.github.com>
2023-02-01 09:09:59 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './HTML/**/*.{html,js}',
+    './ADMIN/**/*.{html,js}',
+    './USER/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
